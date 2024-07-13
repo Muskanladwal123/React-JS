7437c72f55493117711621d735e1e17eda9a0746
Muskan <muskanladwal2002@gmail.com>
2024-07-13 22:53:31 +0530

    episode-01

---
+++ a.js
@@ -0,0 +1,39 @@
+// const heading = React.createElement("h1", {id: "heading1", xyz: "h-one"}, "hello world from React!");
+
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//nested tags
+/* 
+<div id ="parent">
+  <div id = "child">
+    <h1>I'm an h1 tag</h1>
+    <h2>I'm an h2 tag</h2>
+  </div>
+  <div id = "child2">
+    <h1>I'm an h1 tag</h1>
+    <h2>I'm an h2 tag</h2>
+  </div>
+</div> 
+
+ReactElement(Object) => HTML(Browser Understands)
+*/
+
+
+//looks mess -- we will do it using jsx later
+const parent = React.createElement("div", {id:"parent"},[ 
+  React.createElement("div", {id:"child"},
+    React.createElement("h1", {}, "I'm an h1 tag")
+  ),
+  
+  React.createElement("div", {id:"child2"},[
+    //adding 2 elements at the same level under the same parent tag
+    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h3", {}, "I'm an h3 tag"),
+  ])
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
